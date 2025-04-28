6da66d4ace429d9eaac6134ab7510fa34c720b53
MINT <mint_chanit@hotmail.com>
2025-04-28 17:34:00 +0700

    feat: add feature article section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,30 @@
+import { Menu } from "lucide-react";
+ 
 function NavBar() {
-    return (
-        <nav className="flex items-center justify-between py-4 px-8 bg-[#F9F8F6] border-b border-gray-300">
-            <a href="/" className="text-3xl  text-brown-600 font-bold">
-                hh<span className="text-green-500">.</span>
-            </a>
-            <div className="hidden md:flex space-x-4">
-                <a href="/login" className="px-8 py-2  bg-white border border-black rounded-full text-black">Log in</a>
-                <a href="/signup" className="px-8 py-2 bg-black text-white rounded-full hover:bg-gray-600 transition-colors">Sign up</a>
-            </div>
-            <button className="md:hidden ">Menu</button>
-        </nav>
-    );
-}
+   return (
+     <nav className="flex items-center justify-between py-4 px-4 sm:px-8 bg-background border-b border-muted">
+       <a href="/" className="text-2xl font-bold">
+         Thomson P<span className="text-green-400">.</span>
+       </a>
+       <div className="hidden md:flex space-x-4">
+         <a
+           href="/login"
+           className="px-9 py-2 rounded-full text-foreground border border-foreground hover:border-muted-foreground hover:text-muted-foreground transition-colors"
+         >
+           Log in
+         </a>
+         <a
+           href="/signup"
+           className="px-8 py-2 bg-foreground text-white rounded-full hover:bg-muted-foreground transition-colors"
+         >
+           Sign up
+         </a>
+       </div>
+       <button className="md:hidden">
+         <Menu />
+       </button>
+     </nav>
+   );
+ }
 
-export default NavBar;
+ export default NavBar;
